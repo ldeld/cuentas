90c003967b49cabe686439fc224d7c2e4587c765
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2018-01-25 16:39:44 +0100

    generates debts controller

---
+++ app/controllers/debts_controller_controller.rb
@@ -0,0 +1,13 @@
+class DebtsControllerController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'debts_controller/index'
+
+  get 'debts_controller/show'
+
+  get 'debts_controller/new'
+
+  get 'debts_controller/create'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'pages#home'
