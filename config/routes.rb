c90be20692366da9e14409916fb07f1b7cdb6387
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2018-01-25 16:44:38 +0100

    nested routes for debts

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'debts_controller/index'
-
-  get 'debts_controller/show'
-
-  get 'debts_controller/new'
-
-  get 'debts_controller/create'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'pages#home'
   resources :users do
     resources :facturas
+    resources :debts
   end
 end
